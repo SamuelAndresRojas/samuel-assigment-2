0fe42c43500df5458df8f5100cb97d1e194d2031
SamuelAndresRojas <saanrojas@gmail.com>
2022-09-11 22:54:49 +1000

    Progress 1

--- src/Assigment2Application.java
+++ src/Assigment2Application.java
@@ -1,9 +1,31 @@
+import java.util.Random;
+import java.util.Scanner;
 
 public class Assigment2Application {
 
 	public static void main(String[] args) {
-
-			// Assigment 2
+		
+		Scanner scanner = new Scanner(System.in);
+		Random random = new Random();
+		
+		int randomnumber = random.nextInt(100);
+		System.out.println(randomnumber);
+		
+		System.out.println("Hello, Let's see if you guess the number");
+		System.out.println("Pick a number between 1 and 100");
+		
+		int userinput =  scanner.nextInt();
+		if (userinput == randomnumber);
+		System.out.println("You win!!!");
+			while (randomnumber != userinput) {
+			if (userinput == randomnumber);
+			System.out.println("You win!!!");				
+			if (userinput < randomnumber);
+			System.out.println("Please pick a higher number");
+			if (userinput > randomnumber);
+			System.out.println("Please pick a lower number");
+							
+			}
 	}
 
 }
