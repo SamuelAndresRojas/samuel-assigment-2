38942cc9223e5124ebd034a1dcd5480f22d78f2b
SamuelAndresRojas <saanrojas@gmail.com>
2022-09-12 22:53:04 +1000

    debug 1

--- src/Assigment2Application.java
+++ src/Assigment2Application.java
@@ -4,28 +4,33 @@ import java.util.Scanner;
 public class Assigment2Application {
 
 	public static void main(String[] args) {
-		
-		Scanner scanner = new Scanner(System.in);
+		Scanner scanner = new Scanner(System.in);	
 		Random random = new Random();
-		
-		int randomnumber = random.nextInt(100);
+
+		int randomnumber = random.nextInt(100) + 1;
+
 		System.out.println(randomnumber);
-		
+
 		System.out.println("Hello, Let's see if you guess the number");
 		System.out.println("Pick a number between 1 and 100");
-		
-		int userinput =  scanner.nextInt();
-		if (userinput == randomnumber);
-		System.out.println("You win!!!");
-			while (randomnumber != userinput) {
-			if (userinput == randomnumber);
-			System.out.println("You win!!!");				
-			if (userinput < randomnumber);
-			System.out.println("Please pick a higher number");
-			if (userinput > randomnumber);
-			System.out.println("Please pick a lower number");
-							
-			}
-	}
 
+		int tries = 0;	
+		while(tries < 5) {
+			int userInput =  scanner.nextInt();
+			if (userInput == randomnumber) {
+				System.out.println("You win!!!");
+				break;
+			}else if (userInput < randomnumber) {
+				tries++;
+				System.out.println("Please pick a higher number ");
+			} else if (userInput > randomnumber){
+				tries++;
+				System.out.println("Please pick a lower number ");
+			}
+
+		}
+		System.out.println("You lose the number was: " + randomnumber);
+
+		scanner.close();
+	}
 }
